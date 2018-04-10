d7e2c7106f09559f678762f83eee1979779ff92a
maximAtanasov <atanasovmaksim1@gmail.com>
2018-04-10 13:42:24 +0200

    inlined all methods of the class entity

--- ST_engine/src/main/game_manager/level/entity.hpp
+++ ST_engine/src/main/game_manager/level/entity.hpp
@@ -41,7 +41,7 @@ class entity{
         int col_y = 0;
         int mass = 0;
         int offset_x = 0;
-    int offset_y = 0;
+        int offset_y = 0;
         collision_data collision;
         bool affected_by_physics = false;
         bool active = true;
@@ -97,5 +97,181 @@ class entity{
 
 
 };
+
+//GENERAL FUNCTIONS//
+inline Uint64 entity::get_ID(){
+    return ID;
+}
+
+inline void entity::set_active(bool arg){
+    active = arg;
+}
+
+inline bool entity::is_active(){
+    return active;
+}
+
+inline int entity::get_x(){
+    return x;
+}
+
+inline int entity::get_y(){
+    return y;
+}
+
+inline void entity::set_x(int X){
+    x = X;
+    update_collision_box(x, y);
+}
+
+inline void entity::set_y(int Y){
+    y = Y;
+    update_collision_box(x, y);
+}
+
+inline void entity::set_static(bool arg){
+    static_ = arg;
+}
+
+inline bool entity::is_static(){
+    return static_;
+}
+
+inline void entity::set_velocity_x(int arg){
+    velocity_x = arg;
+}
+
+inline void entity::set_velocity_y(int arg){
+    velocity_y = arg;
+}
+
+inline int entity::get_velocity_x(){
+    return velocity_x;
+}
+
+inline int entity::get_velocity_y(){
+    return velocity_y;
+}
+
+//TEXTURE FUNCTIONS//
+
+inline int entity::get_tex_w(){
+    return tex_w;
+}
+
+inline int entity::get_tex_h(){
+    return tex_h;
+}
+
+inline void entity::set_tex_w(int width){
+    tex_w = width;
+}
+
+inline void entity::set_tex_h(int height){
+    tex_h = height;
+}
+
+inline size_t entity::get_texture(){
+    return texture;
+}
+
+inline void entity::set_visible(bool arg){
+    visibility = arg;
+}
+
+inline void entity::set_texture(std::string arg){
+    std::hash<std::string> hash_f;
+    texture = hash_f(arg);
+}
+
+inline bool entity::is_visible(){
+    return visibility;
+}
+
+//PHYSICS FUNTIONS//
+
+inline int entity::get_col_x(){
+    return col_x;
+}
+
+inline int entity::get_col_y(){
+    return col_y;
+}
+
+inline int entity::get_col_x_offset(){
+    return offset_x;
+}
+
+inline int entity::get_col_y_offset(){
+    return offset_y;
+}
+
+inline void entity::set_collision_box(int offsetX, int offsetY, int X, int Y){
+    if(X != 0 && Y != 0){
+        col_x = X - 1;
+        col_y = -Y + 1;
+        offset_x = offsetX;
+        offset_y = offsetY;
+    }else{
+        col_x = 0;
+        col_y = 0;
+        set_affected_by_physics(false);
+    }
+    update_collision_box(x, y);
+}
+
+inline void entity::update_collision_box(int x, int y){
+    collision.left = x + offset_x;
+    collision.right = x + col_x + offset_x;
+    collision.top = y + col_y + offset_y;
+    collision.bottom = y + offset_y;
+}
+
+inline void entity::set_affected_by_physics(bool arg){
+    affected_by_physics = arg;
+}
+
+inline bool entity::is_affected_by_physics(){
+    return affected_by_physics;
+}
+
+inline bool entity::collides(entity other){
+    return !((collision.bottom < other.collision.top) || (collision.top > other.collision.bottom)
+          || (collision.left > other.collision.right) || (collision.right < other.collision.left));
+}
+
+inline void entity::set_mass(int arg){
+    mass = arg;
+}
+
+inline int entity::get_mass(){
+    return mass;
+}
+
+//ANIMATION FUNCTIONS//
+
+inline void entity::set_animation(int arg){
+    animation = arg;
+}
+
+inline int entity::get_animation(){
+    return animation;
+}
+
+inline void entity::set_animation_num(int arg){
+    animation_num = arg;
+}
+
+inline int entity::get_animation_num(){
+    return animation_num;
+}
+
+inline void entity::set_sprite_num(int arg){
+    sprite_num = arg;
+}
+
+inline int entity::get_sprite_num(){
+    return sprite_num;
+}
 
 #endif
