e36efb22f04c47c40f016da95120e30bbfdfbb37
max <atanasovmaksim1@gmail.com>
2018-05-21 10:28:39 +0200

    Fixed typo

--- search/all_9.js
+++ search/all_9.js
@@ -3,7 +3,7 @@ var searchData=
   ['level',['level',['../classST_1_1level.html',1,'ST']]],
   ['level',['level',['../classST_1_1level.html#a8592580d090d80586851cf4a72a937b8',1,'ST::level']]],
   ['level_5fdata',['level_data',['../structST_1_1level__data.html',1,'ST']]],
-  ['light',['light',['../classST_1_1light.html#af57745347b0e2d59c0239c1c8bc15f80',1,'ST::light']]],
+  ['light',['light',['../classST_1_1light.html#a27eb688cc25337fc412a6fa0a162c58a',1,'ST::light']]],
   ['light',['light',['../classST_1_1light.html',1,'ST']]],
   ['load',['load',['../classST_1_1level.html#ac8dab68daa30c11bdc55217ba170ed4c',1,'ST::level']]],
   ['load_5ffile',['load_file',['../classlua__backend.html#a1a9c90d86b894fc04dc8672617c6562e',1,'lua_backend']]],
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['level',['level',['../classST_1_1level.html#a8592580d090d80586851cf4a72a937b8',1,'ST::level']]],
-  ['light',['light',['../classST_1_1light.html#af57745347b0e2d59c0239c1c8bc15f80',1,'ST::light']]],
+  ['light',['light',['../classST_1_1light.html#a27eb688cc25337fc412a6fa0a162c58a',1,'ST::light']]],
   ['load',['load',['../classST_1_1level.html#ac8dab68daa30c11bdc55217ba170ed4c',1,'ST::level']]],
   ['load_5ffile',['load_file',['../classlua__backend.html#a1a9c90d86b894fc04dc8672617c6562e',1,'lua_backend']]]
 ];
