b69869a778ea9b4bf0198b86f126f55c6cc9b6ae
max <atanasovmaksim1@gmail.com>
2018-08-09 21:33:56 +0200

    updated docs

--- search/all_9.js
+++ search/all_9.js
@@ -8,5 +8,6 @@ var searchData=
   ['light',['light',['../classST_1_1light.html#a27eb688cc25337fc412a6fa0a162c58a',1,'ST::light']]],
   ['load',['load',['../classST_1_1level.html#ac8dab68daa30c11bdc55217ba170ed4c',1,'ST::level']]],
   ['load_5ffile',['load_file',['../classlua__backend.html#a1a9c90d86b894fc04dc8672617c6562e',1,'lua_backend']]],
-  ['lua_5fbackend',['lua_backend',['../classlua__backend.html',1,'']]]
+  ['lua_5fbackend',['lua_backend',['../classlua__backend.html',1,'']]],
+  ['lua_5fbackend_5ftest',['lua_backend_test',['../classlua__backend__test.html',1,'']]]
 ];
--- search/classes_8.js
+++ search/classes_8.js
@@ -4,5 +4,6 @@ var searchData=
   ['level_5fdata',['level_data',['../structST_1_1level__data.html',1,'ST']]],
   ['level_5ftests',['level_tests',['../classlevel__tests.html',1,'']]],
   ['light',['light',['../classST_1_1light.html',1,'ST']]],
-  ['lua_5fbackend',['lua_backend',['../classlua__backend.html',1,'']]]
+  ['lua_5fbackend',['lua_backend',['../classlua__backend.html',1,'']]],
+  ['lua_5fbackend_5ftest',['lua_backend_test',['../classlua__backend__test.html',1,'']]]
 ];
