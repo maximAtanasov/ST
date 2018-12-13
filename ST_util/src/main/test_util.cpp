abc5339801f7eac6b709e32471efb773dedd6cb3
Maksim Atanasov <atanasovmaksim1@gmail.com>
2018-12-13 18:18:43 +0100

    circle ci test mock video

--- ST_util/src/main/test_util.cpp
+++ ST_util/src/main/test_util.cpp
@@ -19,9 +19,9 @@
 
 void initialize_SDL(){
 //Dummy video device when testing
-	if (strncmp(std::getenv("SDL_VIDEODRIVER"), "dummy", 6)==0) {
-		putenv((std::string("SDL_VIDEODRIVER=") + std::string(std::getenv("SDL_VIDEODRIVER"))).c_str());
-	}
+#ifdef SDL_VIDEODRIVER
+    putenv(const_cast<char*>("SDL_VIDEODRIVER=dummy"));
+#endif
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
         exit(1);
